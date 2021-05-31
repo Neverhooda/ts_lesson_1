e85774d2523c8804c777f5f4079f40b4c5e0bccf
Alexey Klimenko <ank@tomsksoft.com>
2021-06-01 00:30:58 +0700

    update for build with wsl2 on win

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class LibtsConan(ConanFile):
     name = "libts"
-    version = "1.0"
+    version = "1.2"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
@@ -13,11 +13,11 @@ class LibtsConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = {"shared": False}
     generators = "cmake"
-    exports_sources = "src/*"
+    exports_sources = ["src/*", "CMakeLists.txt", "test_package/*"]
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure(source_folder='.')
         cmake.build()
 
         # Explicit way:
